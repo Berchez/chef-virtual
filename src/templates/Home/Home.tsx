69ace4c06ea121c7986d530e23bea86af4de26dc
Berchez <walterfelipeberchez@outlook.com>
2023-06-27 00:38:53 -0300

    style(front): mount the home

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/Button";
+import { Footer } from "@/components/Footer";
+import { Menu } from "@/components/Menu/Menu";
 import React from "react";
 
 export const Home = () => {
   return (
-    <div className="bg-cyan-200">
-      <p>Initial</p>
+    <div className="flex flex-col w-screen h-screen">
+      <Menu />
+      <div className="flex flex-col px-16 grow">
+        <div className="grid grid-cols-2 h-full w-full">
+          <span className="bg-red-600 h-full"></span>
+          <span className="bg-red-600 h-full"></span>
+        </div>
+        <Button label="Teste" />
+      </div>
+      <Footer />
     </div>
   );
 };
