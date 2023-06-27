00b17fa1d354b6be70296604a4a8b223459d582e
Berchez <walterfelipeberchez@outlook.com>
2023-06-27 00:38:15 -0300

    style(front): size of menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,7 +4,7 @@ import logo from "../../../public/assets/logo.png";
 
 export const Menu = () => {
   return (
-    <nav className="h-11 text-gray-900 bg-white flex flex-row justify-between items-center px-16 border-b-2 border-solid border-b-gray-300">
+    <nav className="h-11   text-gray-900 bg-white flex flex-row justify-between items-center px-16 border-b-2 border-solid border-b-gray-300">
       <div className="flex flex-row gap-x-2 items-center font-semibold">
         <Image alt="logo do chef virtual" src={logo} width={32} />
         <p>Chef Virtual</p>
