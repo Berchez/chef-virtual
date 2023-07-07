02163cb326c97afa0e3f1533d7d5c44389a43306
Berchez <walterfelipeberchez@outlook.com>
2023-07-07 03:27:48 -0300

    feat(front): showing api response

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -1,23 +1,55 @@
-import axios from 'axios';
-import { IngredientProps } from '../components/Ingredient/Ingredient';
-
-
+import axios from "axios";
+import { IngredientProps } from "../components/Ingredient/Ingredient";
 
 export const customAxios = axios.create({
-  headers:
-  {
+  headers: {
     "Content-Type": "application/json",
     Authorization: "Bearer " + process.env.OPENAI_API_KEY,
   },
 });
 
-export const sendIngredientsGPT = async (listIngredients: IngredientProps[]) => {
+export const sendIngredientsGPT = async (
+  listIngredients: IngredientProps[]
+) => {
+  const stringIngredients = listIngredients
+    .map(function (obj) {
+      return obj.name;
+    })
+    .join(", ");
+
   const params = {
-    prompt: "Liste uma série de receitas deliciosas da culinária brasileira que utilizem os ingredientes: ovo, batata, feijao, e arroz",
-    model: "text-davinci-003",
+    prompt: `Liste uma série de receitas deliciosas da culinária
+    brasileira que utilizem os ingredientes: ${stringIngredients}`,
+    model: "gpt-3.5-turbo-0613",
     max_tokens: 10,
     temperature: 0,
   };
-    const response = await customAxios.post('https://api.openai.com/v1/completions', params)
-    return response.data.choices[0].text.trim();
+  return `Aqui estão algumas receitas deliciosas da culinária brasileira que utilizam os ingredientes batata, ovo, arroz e carne suína:
+          Feijoada
+          Ingredientes:
+          500g de carne suína (costela, lombo, pé, orelha, rabo)
+          250g de carne seca
+          200g de linguiça calabresa
+          200g de linguiça portuguesa
+          2 xícaras de feijão preto
+          2 batatas grandes
+          4 ovos cozidos
+          3 dentes de alho
+          1 cebola grande
+          Cheiro-verde a gosto
+          Sal e pimenta a gosto
+          Modo de preparo:
+          Cozinhe as carnes suína e seca separadamente até ficarem macias.
+          Em outra panela, cozinhe o feijão preto até ficar macio.
+          Em uma panela grande, refogue o alho e a cebola.
+          Adicione as carnes cozidas e cortadas em pedaços.
+          Acrescente as linguiças em rodelas e as batatas cortadas em cubos.
+          Adicione o feijão cozido e cozinhe por mais alguns minutos.
+          Tempere com sal, pimenta e cheiro-verde.
+          Sirva com arroz branco e os ovos cozidos.`;
+  // const response = await customAxios.post(
+  //   "	https://api.openai.com/v1/chat/completions",
+  //   params
+  // );
+  // return response.data.choices[0].text.trim();
 };
--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,23 +1,13 @@
-import { Button } from "@/components/Button";
 import { Footer } from "@/components/Footer";
 import { Menu } from "@/components/Menu/Menu";
 import React from "react";
-import { TwoHomeGrids } from "./templates/TwoHomeGrids/TwoHomeGrids";
+import { HomeContent } from "./templates/HomeContent";
 
 export const Home = () => {
-
   return (
     <div className="flex flex-col w-screen h-screen max-h-screen bg-gray-200">
       <Menu />
-      <div className="flex flex-col grow px-16 py-8 h-full min-h-0">
-        <div className="grid grid-cols-3 w-full gap-x-8 h-full min-h-0">
-          <TwoHomeGrids />
-        </div>
-        <Button
-          label="Buscar Receitas"
-          className="self-end mt-4 w-52"
-        />
-      </div>
+      <HomeContent />
       <Footer />
     </div>
   );
